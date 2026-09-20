321965143ee8121b67e4f8503d2f9569cfa5194c
Nitin Kandpal <nitinkandpalco@gmail.com>
2026-09-20 21:21:01 +0530

    Remove all files

--- src/algorithms/pathfinding/index.ts
+++
@@ -1,6 +0,0 @@
-/** Grid pathfinding and maze generation. */
-import { gridAStar, gridBfs, gridBidirectional, gridDfs, gridDijkstra, gridGreedy } from './search';
-import { mazeBacktracker, mazeKruskal, mazePrim } from './maze';
-
-/** Pathfinding and maze definitions in sidebar order. */
-export const PATHFINDING = [gridBfs, gridDfs, gridDijkstra, gridAStar, gridGreedy, gridBidirectional, mazeBacktracker, mazePrim, mazeKruskal];
--- src/algorithms/strings/index.ts
+++
@@ -1,5 +0,0 @@
-/** String algorithms. */
-import { kmp, rabinKarp, zAlgorithm } from './matching';
-
-/** String definitions in sidebar order. */
-export const STRINGS = [kmp, rabinKarp, zAlgorithm];
